8b5dc1f51cc66cc29be2b6d8f333a3a86bab3d12
Nixtov <nixtov@gmail.com>
2017-09-29 19:25:14 +0300

    Program.cs

---
+++ Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheatrePromotions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string day = Console.ReadLine().ToLower();
+            int age = int.Parse(Console.ReadLine());
+
+            switch (day)
+            {
+                case "weekday":
+                    if (0 <= age && age <= 18 || 64 < age && age <= 122)
+                    {
+                        Console.WriteLine("12$");
+                    }
+                    else if (18 < age && age <= 64)
+                    {
+                        Console.WriteLine("18$");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error!");
+                    }
+                    break;
+                case "weekend":
+                    if (0 <= age && age <= 18 || 64 < age && age <= 122)
+                    {
+                        Console.WriteLine("15$");
+                    }
+                    else if (18 < age && age <= 64)
+                    {
+                        Console.WriteLine("20$");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error!");
+                    }
+                    break;
+                case "holiday":
+                    if (0 <= age && age <= 18)
+                    {
+                        Console.WriteLine("5$");
+                    }
+                    else if (18 < age && age <= 64)
+                    {
+                        Console.WriteLine("12$");
+                    }
+                    else if (64 < age && age <= 122)
+                    {
+                        Console.WriteLine("10$");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error!");
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Error!");
+                    break;
+            }
+        }
+    }
+}
